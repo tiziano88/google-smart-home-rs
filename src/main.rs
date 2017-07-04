852b921b2a4814253d37f6083a7dbb0b89738fa4
Tiziano Santoro <tiziano88@gmail.com>
2017-07-04 21:28:34 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate iron;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
 fn main() {
     println!("Hello, world!");
+    let mut control = Router::new();
+    control.get("/auth",
+             |req: &mut Request| Ok(Response::with((status::Ok, "auth"))),
+             "auth")
+        .get("/",
+             |req: &mut Request| Ok(Response::with((status::Ok, "index"))),
+             "index");
+    Iron::new(control)
+        .http("127.0.0.1:3000")
+        .unwrap();
 }
