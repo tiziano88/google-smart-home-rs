8258936464c7ec16838839c6335eeb13c2c162b9
Tiziano Santoro <tiziano88@gmail.com>
2017-07-19 17:42:08 +0100

    req

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use iron::prelude::*;
 use iron::status;
 use router::Router;
 use url::Url;
+use std::sync::Mutex;
 
 mod google_actions;
 use google_actions::*;
@@ -29,30 +30,106 @@ mod smart_home;
 use smart_home::*;
 
 struct Hub {
-    lights: Vec<Light>,
-}
-
-impl Hub {
-    fn action_handler(&mut self, req: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "xxx")))
-    }
+    light: Mutex<Light>,
 }
 
 impl Handler for Hub {
     fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "xxx")))
+        let mut light = self.light.lock().unwrap();
+
+        let mut body = String::new();
+        req.body.read_to_string(&mut body).unwrap();
+        let action_request: ActionRequest = serde_json::from_str(&body).unwrap();
+
+        println!("action_request: {:?}", action_request);
+
+        for input in &action_request.inputs {
+            if input.intent == "action.devices.SYNC" {
+                let response = SyncResponse {
+                    request_id: action_request.request_id.clone(),
+                    payload: SyncResponsePayload {
+                        agent_user_id: "1111".to_string(),
+                        devices: vec![SyncResponseDevice {
+                                          id: light.id.clone(),
+                                          type_: "action.devices.types.LIGHT".to_string(),
+                                          traits: vec!["action.devices.traits.OnOff".to_string()],
+                                          name: Name {
+                                              default_name: vec!["foo".to_string()],
+                                              name: Some(light.name.clone()),
+                                              nicknames: vec![],
+                                          },
+                                          will_report_state: false,
+                                          device_info: None,
+                                          room_hint: None,
+                                          structure_hint: None,
+                                      }],
+                    },
+                };
+                let res = serde_json::to_string(&response).unwrap_or("".to_string());
+                let mut rsp = Response::with((status::Ok, res));
+                rsp.headers.set(ContentType::json());
+                return Ok(rsp);
+            } else if input.intent == "action.devices.QUERY" {
+                let light_status = light.get_status();
+                let response = QueryResponse {
+                    request_id: action_request.request_id.clone(),
+                    payload: QueryResponsePayload {
+                        devices: btreemap!{
+                        "123".to_string() => DeviceStates {
+                            online: true,
+                            on: light_status.on,
+                            brightness: light_status.brightness,
+                            color: Color {
+                                name: "red".to_string(),
+                                temperature: 0,
+                                spectrum_rgb: light_status.spectrum_rgb,
+                            },
+                        }
+                    },
+                    },
+                };
+                let res = serde_json::to_string(&response).unwrap_or("".to_string());
+                let mut rsp = Response::with((status::Ok, res));
+                rsp.headers.set(ContentType::json());
+                return Ok(rsp);
+            } else if input.intent == "action.devices.EXECUTE" {
+                let light_status = light.get_status();
+                light.set_status(light_status);
+
+                let response = ExecuteResponse {
+                    request_id: action_request.request_id.clone(),
+                    payload: ExecuteResponsePayload {
+                        error_code: Some("ERROR".to_string()),
+                        debug_string: Some("TODO".to_string()),
+                        commands: vec![],
+                    },
+                };
+                let res = serde_json::to_string(&response).unwrap_or("".to_string());
+                let mut rsp = Response::with((status::Ok, res));
+                rsp.headers.set(ContentType::json());
+                return Ok(rsp);
+            }
+        }
+
+        let mut rsp = Response::with((status::Ok, "ACTION"));
+        rsp.headers.set(ContentType::json());
+        Ok(rsp)
     }
 }
 
 fn main() {
-    let mut hub = Hub { lights: vec![] };
+    let mut hub = Hub {
+        light: Mutex::new(Light {
+            id: "11".to_string(),
+            name: "11".to_string(),
+        }),
+    };
     println!("Hello, world!");
     let mut control = Router::new();
     control.get("/auth", auth_handler, "auth")
         .post("/token", token_handler, "token")
         .get("/login", login_handler, "login")
-        .post("/action", action_handler, "action")
-        .post("/xxx", hub, "action")
+        .post("/action", hub, "action")
         .get("/", index_handler, "index");
     Iron::new(control)
         .http("0.0.0.0:1234")
@@ -117,82 +194,6 @@ fn login_handler(req: &mut Request) -> IronResult<Response> {
     let password = map.find(&["password"]);
 
     Ok(Response::with((status::Ok, "login")))
-}
-
-fn action_handler(req: &mut Request) -> IronResult<Response> {
-    let mut body = String::new();
-    req.body.read_to_string(&mut body).unwrap();
-    let action_request: ActionRequest = serde_json::from_str(&body).unwrap();
-
-    println!("action_request: {:?}", action_request);
-
-    for input in &action_request.inputs {
-        if input.intent == "action.devices.SYNC" {
-            let response = SyncResponse {
-                request_id: action_request.request_id.clone(),
-                payload: SyncResponsePayload {
-                    agent_user_id: "1111".to_string(),
-                    devices: vec![SyncResponseDevice {
-                                      id: "123".to_string(),
-                                      type_: "action.devices.types.LIGHT".to_string(),
-                                      traits: vec!["action.devices.traits.OnOff".to_string()],
-                                      name: Name {
-                                          default_name: vec!["foo".to_string()],
-                                          name: Some("foo".to_string()),
-                                          nicknames: vec![],
-                                      },
-                                      will_report_state: false,
-                                      device_info: None,
-                                      room_hint: None,
-                                      structure_hint: None,
-                                  }],
-                },
-            };
-            let res = serde_json::to_string(&response).unwrap_or("".to_string());
-            let mut rsp = Response::with((status::Ok, res));
-            rsp.headers.set(ContentType::json());
-            return Ok(rsp);
-        } else if input.intent == "action.devices.QUERY" {
-            let response = QueryResponse {
-                request_id: action_request.request_id.clone(),
-                payload: QueryResponsePayload {
-                    devices: btreemap!{
-                        "123".to_string() => DeviceStates {
-                            online: true,
-                            on: true,
-                            brightness: 10,
-                            color: Color {
-                                name: "red".to_string(),
-                                temperature: 0,
-                                spectrum_rgb: 0,
-                            },
-                        }
-                    },
-                },
-            };
-            let res = serde_json::to_string(&response).unwrap_or("".to_string());
-            let mut rsp = Response::with((status::Ok, res));
-            rsp.headers.set(ContentType::json());
-            return Ok(rsp);
-        } else if input.intent == "action.devices.EXECUTE" {
-            let response = ExecuteResponse {
-                request_id: action_request.request_id.clone(),
-                payload: ExecuteResponsePayload {
-                    error_code: Some("ERROR".to_string()),
-                    debug_string: Some("TODO".to_string()),
-                    commands: vec![],
-                },
-            };
-            let res = serde_json::to_string(&response).unwrap_or("".to_string());
-            let mut rsp = Response::with((status::Ok, res));
-            rsp.headers.set(ContentType::json());
-            return Ok(rsp);
-        }
-    }
-
-    let mut rsp = Response::with((status::Ok, "ACTION"));
-    rsp.headers.set(ContentType::json());
-    Ok(rsp)
 }
 
 fn index_handler(req: &mut Request) -> IronResult<Response> {
