70fee89da5f6a7c8ed547c477f22c1d506df8bbb
Tiziano Santoro <tiziano88@gmail.com>
2017-07-29 21:08:35 -0700

    oauth struct

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,79 @@ use google_actions::*;
 mod smart_home;
 use smart_home::*;
 
+struct OAuth {
+    auth: OAuthAuth,
+    token: OAuthToken,
+}
+
+impl OAuth {
+    fn new() -> OAuth {
+        OAuth {
+            auth: OAuthAuth {},
+            token: OAuthToken {},
+        }
+    }
+}
+
+struct OAuthAuth {}
+
+impl Handler for OAuthAuth {
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        let map = req.get_ref::<params::Params>().unwrap();
+
+        let _ = map.find(&["response_type"]);
+        let _ = map.find(&["client_id"]);
+        let redirect_uri = map.find(&["redirect_uri"]);
+        let scope = map.find(&["scope"]);
+        let state = map.find(&["state"]);
+
+        println!("uri: {:?}", redirect_uri);
+        println!("scope: {:?}", scope);
+
+        let s = match state {
+            Some(&params::Value::String(ref x)) => x,
+            _ => "",
+        };
+
+        let u = match redirect_uri {
+            Some(&params::Value::String(ref x)) => {
+                let mut url = Url::parse(&x).unwrap();
+                url.set_query(Some(&format!("code=123&state={}", s)));
+                url
+            }
+            _ => Url::parse("").unwrap(),
+        };
+
+        let uu = iron::Url::from_generic_url(u).unwrap();
+
+        Ok(Response::with((status::Found, Redirect(uu))))
+    }
+}
+
+struct OAuthToken {}
+
+impl Handler for OAuthToken {
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        let map = req.get_ref::<params::Params>().unwrap();
+
+        let _ = map.find(&["grant_type"]);
+        let _ = map.find(&["code"]);
+        let _ = map.find(&["redirect_uri"]);
+        let _ = map.find(&["client_id"]);
+
+        let auth_response = AuthResponse {
+            token_type: "bearer".to_string(),
+            access_token: "xxx".to_string(),
+            refresh_token: "yyy".to_string(),
+            expires_in: 1000000,
+        };
+
+        let res = serde_json::to_string(&auth_response).unwrap_or("".to_string());
+
+        Ok(Response::with((status::Ok, res)))
+    }
+}
+
 struct Hub {
     devices: Mutex<Vec<Device>>,
 }
@@ -248,9 +321,10 @@ fn main() {
                                      mote: mote::Mote::new("/dev/ttyACM0"),
                                  })]),
     };
+    let oauth = OAuth::new();
     let mut control = Router::new();
-    control.get("/auth", auth_handler, "auth")
-        .post("/token", token_handler, "token")
+    control.get("/auth", oauth.auth, "auth")
+        .post("/token", oauth.token, "token")
         .get("/login", login_handler, "login")
         .post("/action", hub, "post action")
         .get("/action", get_action_handler, "get action")
@@ -259,57 +333,6 @@ fn main() {
     Iron::new(control)
         .http("0.0.0.0:1234")
         .unwrap();
-}
-
-fn auth_handler(req: &mut Request) -> IronResult<Response> {
-    let map = req.get_ref::<params::Params>().unwrap();
-
-    let _ = map.find(&["response_type"]);
-    let _ = map.find(&["client_id"]);
-    let redirect_uri = map.find(&["redirect_uri"]);
-    let scope = map.find(&["scope"]);
-    let state = map.find(&["state"]);
-
-    println!("uri: {:?}", redirect_uri);
-    println!("scope: {:?}", scope);
-
-    let s = match state {
-        Some(&params::Value::String(ref x)) => x,
-        _ => "",
-    };
-
-    let u = match redirect_uri {
-        Some(&params::Value::String(ref x)) => {
-            let mut url = Url::parse(&x).unwrap();
-            url.set_query(Some(&format!("code=123&state={}", s)));
-            url
-        }
-        _ => Url::parse("").unwrap(),
-    };
-
-    let uu = iron::Url::from_generic_url(u).unwrap();
-
-    Ok(Response::with((status::Found, Redirect(uu))))
-}
-
-fn token_handler(req: &mut Request) -> IronResult<Response> {
-    let map = req.get_ref::<params::Params>().unwrap();
-
-    let _ = map.find(&["grant_type"]);
-    let _ = map.find(&["code"]);
-    let _ = map.find(&["redirect_uri"]);
-    let _ = map.find(&["client_id"]);
-
-    let auth_response = AuthResponse {
-        token_type: "bearer".to_string(),
-        access_token: "xxx".to_string(),
-        refresh_token: "yyy".to_string(),
-        expires_in: 1000000,
-    };
-
-    let res = serde_json::to_string(&auth_response).unwrap_or("".to_string());
-
-    Ok(Response::with((status::Ok, res)))
 }
 
 fn login_handler(req: &mut Request) -> IronResult<Response> {
